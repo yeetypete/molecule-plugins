02e1e8e7fd092ec092ed3964d34b53069f5e132a
Peter Siegel <psiegel2000@icloud.com>
2025-01-30 23:14:07 +0100

    feat: copy existing molecule lxd plugin

---
+++ src/molecule_plugins/lxd/test/test_driver.py
@@ -0,0 +1,7 @@
+"""Unit tests"""
+
+from molecule import api
+
+
+def test_driver_is_detected():
+    assert "lxd" in [str(d) for d in api.drivers()]
---
+++ src/molecule_plugins/lxd/test/test_func.py
@@ -0,0 +1,93 @@
+#  Copyright (c) 2015-2018 Cisco Systems, Inc.
+#  Copyright (c) 2018 Red Hat, Inc.
+#
+#  Permission is hereby granted, free of charge, to any person obtaining a copy
+#  of this software and associated documentation files (the "Software"), to
+#  deal in the Software without restriction, including without limitation the
+#  rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+#  sell copies of the Software, and to permit persons to whom the Software is
+#  furnished to do so, subject to the following conditions:
+#
+#  The above copyright notice and this permission notice shall be included in
+#  all copies or substantial portions of the Software.
+#
+#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+#  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+#  DEALINGS IN THE SOFTWARE.
+"""Functional tests"""
+
+import os
+import pathlib
+
+import pytest
+
+from molecule import util
+from molecule.test.conftest import change_dir_to
+from molecule.util import run_command
+
+
+def test_command_init_scenario(tmp_path: pathlib.Path):
+    """Verify that init scenario works."""
+    scenario_name = "default"
+
+    with change_dir_to(tmp_path):
+        scenario_directory = tmp_path / "molecule" / scenario_name
+        cmd = [
+            "molecule",
+            "init",
+            "scenario",
+            scenario_name,
+            "--driver-name",
+            "lxd",
+        ]
+        result = run_command(cmd)
+        assert result.returncode == 0
+
+        assert scenario_directory.exists()
+
+        # run molecule reset as this may clean some leftovers from other
+        # test runs and also ensure that reset works.
+        result = run_command(["molecule", "reset"])  # default scenario
+        assert result.returncode == 0
+
+        result = run_command(["molecule", "reset", "-s", scenario_name])
+        assert result.returncode == 0
+
+        cmd = ["molecule", "--debug", "test", "-s", scenario_name]
+        result = run_command(cmd)
+        assert result.returncode == 0
+
+
+@pytest.mark.parametrize(
+    "scenario",
+    ["default", "basic-packages"],
+)
+def test_container_scenario(tmp_path: pathlib.Path, scenario):
+    scenario_directory = os.path.join(
+        os.path.dirname(util.abs_path(__file__)), "scenarios"
+    )
+
+    with change_dir_to(scenario_directory):
+        cmd = ["molecule", "--debug", "test", "-s", scenario]
+        result = run_command(cmd)
+        assert result.returncode == 0
+
+
+@pytest.mark.parametrize(
+    "scenario",
+    ["virtual-machine"],
+)
+@pytest.mark.skipif(not os.path.exists("/dev/kvm"), reason="requires KVM")
+def test_vm_scenario(tmp_path: pathlib.Path, scenario):
+    scenario_directory = os.path.join(
+        os.path.dirname(util.abs_path(__file__)), "scenarios"
+    )
+
+    with change_dir_to(scenario_directory):
+        cmd = ["molecule", "--debug", "test", "-s", scenario]
+        result = run_command(cmd)
+        assert result.returncode == 0
